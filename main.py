442af72d18a3e1c9b1151e34294fc98071dd06c9
LuciferUzami <praveentechci2024@gmail.com>
2024-09-30 16:33:59 +0530

    working on progress

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
+import time
+import turtle
 from turtle import Turtle, Screen
 
+turtle.tracer(0)
 #  Create Objects
-tom = Turtle()
+tom_head = Turtle()
 window = Screen()
 
 #  Set window background
 window.bgcolor("black")
 
+#
+snake_body = []
 #  Snake movements
-def snakeMovement(snake_head):
-    pass
+def moveRight():
+    tom_head.right(90)
+def moveLeft():
+    tom_head.left(-90)
 
 #  Snake head
-tom.penup()
-tom.shape("circle")
-tom.shapesize(1.5, 1.5, 1)
-tom.color("white")
+tom_head.penup()
+tom_head.shape("circle")
+tom_head.shapesize(1.5, 1.5, 1)
+tom_head.color("white")
+#  Store on array
+snake_body.append(tom_head)
+
 
 #  Create Snake Body
 x_axis = -21
@@ -27,6 +37,24 @@ for body in range(3):
     tom_body.color("white")
     tom_body.goto(x_axis, y_axit)
     x_axis += -21
+    snake_body.append(tom_body)
+#
+window.listen()
+window.onkeypress(moveRight, "Right")
+window.onkeypress(moveLeft, "Left")
+
+#  Movies
+while True:
+    turtle.update()
+    time.sleep(0.1)
+    for bodies in snake_body:
+        bodies.speed(1)
+        bodies.forward(15)
+
+
+
+#  tracer off
+turtle.update()
 
 
 # Exit
